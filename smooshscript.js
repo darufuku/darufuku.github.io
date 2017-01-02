6ff471b9aff3c6ba8689c8ff293d7fee8ba44d7a
smooshfu <jessiefu.98@gmail.com>
2017-01-01 22:17:15 -0500

    trying to add fonts

--- smooshscript.js
+++ smooshscript.js
@@ -1,15 +1,15 @@
 $('.whitebox').mouseover(function(){
-    if((this).attr('id') == 'box1'){
+    if(('.whitebox').attr('id') == 'box1'){
         $('#blurb1').fadeTo(500, 0).css('display', 'inline-block');
-    }else if((this).attr('id') == 'box2'){
+    }else if(('.whitebox').attr('id') == 'box2'){
          $('#blurb2').fadeTo(500, 0).css('display', 'inline-block');
     }
     //$('.blurb').fadeTo(500, 0).css('display', 'inline-block');
 });
 $('.whitebox').mouseout(function(){
-    if((this).attr('id') == 'box1'){
+    if(('.whitebox').attr('id') == 'box1'){
         $('#blurb1').fadeTo(500, 1).css('display', 'none');
-    }else if((this).attr('id') == 'box2'){
+    }else if(('.whitebox').attr('id') == 'box2'){
          $('#blurb2').fadeTo(500, 1).css('display', 'none');
     }
    //$('.blurb').fadeTo(500, 1).css('display', 'none');
