84b90dd179cc8e6ca1291536f8abcf30197794f0
smooshfu <jessiefu.98@gmail.com>
2017-01-01 21:41:20 -0500

    trying to find chidren

--- smooshscript.js
+++ smooshscript.js
@@ -1,6 +1,8 @@
 $('.whitebox').mouseover(function(){
-    $('.blurb').fadeTo(0,1000).css('display', 'inline-block');
+    //$('.blurb').fadeTo(500, 0).css('display', 'inline-block');
+    $((this).children('.blurb')).fadeTo(500, 0).css('display', 'inline-block');
 });
 $('.whitebox').mouseout(function(){
-    $('.blurb').fadeTo(1,1000).css('display', 'none');
+   // $('.blurb').fadeTo(500, 1).css('display', 'none');
+    $((this).children('.blurb')).fadeTo(500, 1).css('display', 'none');
 });
