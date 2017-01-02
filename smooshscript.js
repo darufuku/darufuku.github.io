f2c35f755764193e2ab9299cb7034dd212b94528
smooshfu <jessiefu.98@gmail.com>
2017-01-01 22:36:59 -0500

    final fix to dropdown

--- smooshscript.js
+++ smooshscript.js
@@ -1,16 +1,14 @@
-$('.whitebox').mouseover(function(){
-    if(('.whitebox').attr('id') == 'box1'){
-        $('#blurb1').fadeTo(500, 0).css('display', 'inline-block');
-    }else if(('.whitebox').attr('id') == 'box2'){
-         $('#blurb2').fadeTo(500, 0).css('display', 'inline-block');
-    }
-    //$('.blurb').fadeTo(500, 0).css('display', 'inline-block');
+$('#box1').mouseover(function(){
+    $('#blurb1').fadeTo(500, 0).css('display', 'inline-block');
 });
-$('.whitebox').mouseout(function(){
-    if(('.whitebox').attr('id') == 'box1'){
-        $('#blurb1').fadeTo(500, 1).css('display', 'none');
-    }else if(('.whitebox').attr('id') == 'box2'){
-         $('#blurb2').fadeTo(500, 1).css('display', 'none');
-    }
-   //$('.blurb').fadeTo(500, 1).css('display', 'none');
+$('#box1').mouseout(function(){
+   $('#blurb1').fadeTo(500, 1).css('display', 'none');
 });
+
+$('#box2').mouseover(function(){
+    $('#blurb2').fadeTo(500, 0).css('display', 'inline-block');
+});
+$('#box2').mouseout(function(){
+   $('#blurb2').fadeTo(500, 1).css('display', 'none');
+});
+
