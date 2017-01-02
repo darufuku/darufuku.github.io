e4cf2307c59efa39766929556e681860712f944d
smooshfu <jessiefu.98@gmail.com>
2017-01-01 21:00:24 -0500

    added this

--- smooshscript.js
+++ smooshscript.js
@@ -1,7 +1,6 @@
 $('.whitebox').mouseover(function(){
-    $('.blurb').fadeIn(500);
-    //$('.blurb').css('display', 'inline-block');
+    $(this.'.blurb').css('display', 'inline-block');
 })
 $('.whitebox').mouseout(function(){
-    $('.blurb').fadeOut(500);
+    $(this.'.blurb').css('display', 'none');
 })
