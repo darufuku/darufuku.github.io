8af880f57d8a4a7df0c32b9ea492fdb288d61db1
smooshfu <jessiefu.98@gmail.com>
2017-01-01 23:02:27 -0500

    revert

--- smooshscript.js
+++ smooshscript.js
@@ -1,14 +1,15 @@
+
 $('#box1').mouseenter(function(){
-    $('#blurb1').fadeTo(500, 0).css('display', 'inline-block');
+    $('#blurb1').css('display', 'inline-block');
 });
 $('#box1').mouseleave(function(){
-   $('#blurb1').fadeTo(500, 1).css('display', 'none');
+   $('#blurb1').css('display', 'none');
 });
 
 $('#box2').mouseenter(function(){
-    $('#blurb2').fadeTo(500, 0).css('display', 'inline-block');
+    $('#blurb2').css('display', 'inline-block');
 });
 $('#box2').mouseleave(function(){
-   $('#blurb2').fadeTo(500, 1).css('display', 'none');
+   $('#blurb2').css('display', 'none');
 });
 
