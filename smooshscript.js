c9f959f2db7e14be8d37971152ee3fe4d1ad0f8f
smooshfu <jessiefu.98@gmail.com>
2017-01-03 00:08:38 -0500

    document.ready added

--- smooshscript.js
+++ smooshscript.js
@@ -1,15 +1,17 @@
+$(document).ready(function(){
+    $('#box1').mouseenter(function(){
+        $('#blurb1').css('display', 'inline-block');
+    });
+    $('#box1').mouseleave(function(){
+       $('#blurb1').css('display', 'none');
+    });
 
-$('#box1').mouseenter(function(){
-    $('#blurb1').css('display', 'inline-block');
-});
-$('#box1').mouseleave(function(){
-   $('#blurb1').css('display', 'none');
+    $('#box2').mouseenter(function(){
+        $('#blurb2').css('display', 'inline-block');
+    });
+    $('#box2').mouseleave(function(){
+       $('#blurb2').css('display', 'none');
+    });  
 });
 
-$('#box2').mouseenter(function(){
-    $('#blurb2').css('display', 'inline-block');
-});
-$('#box2').mouseleave(function(){
-   $('#blurb2').css('display', 'none');
-});
 
